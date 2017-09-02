1f96065cb5219e3091094ed457f54eb654186e1f
James Johnson <james_johnson@mcad.edu>
2017-09-02 01:01:41 -0500

    wip

--- cli.js
+++ cli.js
@@ -1,35 +1,8 @@
 #!/usr/bin/env node
 
-const path = require('path');
-const os = require('os');
-const fs = require('fs');
-const { promisify } = require('util');
 const inquirer = require('inquirer');
 const SimpleLDAPSearch = require('simple-ldap-search').default;
-
-const configQuestions = [
-  {
-    type: 'input',
-    name: 'url',
-    message: 'LDAP url, e.g. `ldaps://ldap.example.org`?',
-  },
-  {
-    type: 'input',
-    name: 'base',
-    message: 'base, e.g. `dc=accounts,dc=ldap,dc=example,dc=org`?',
-  },
-  {
-    type: 'input',
-    name: 'dn',
-    message: 'dn, e.g. `uid=artvandelay,dc=users,dc=accounts,dc=ldap,dc=example,dc=org`',
-  },
-  {
-    type: 'password',
-    name: 'password',
-    mask: '*',
-    message: 'Password?',
-  },
-];
+const getConfig = require('./lib/getConfig');
 
 const filterQuestions = [
   {
@@ -38,24 +11,6 @@ const filterQuestions = [
     message: 'ldap search (e.g. `uid=artvandelay`)?',
   },
 ];
-
-async function getConfig() {
-  const readFileAsync = promisify(fs.readFile);
-  const writeFileAsync = promisify(fs.writeFile);
-  const filename = path.resolve(os.homedir(), '.ldaplooksee.json');
-
-  try {
-    const contents = await readFileAsync(filename, 'utf8');
-    return JSON.parse(contents);
-  } catch (err) {
-    // console.log(err);
-    console.log('No saved config.');
-    const answers = await inquirer.prompt(configQuestions);
-    console.log('Saving config ... ');
-    await writeFileAsync(filename, JSON.stringify(answers), { mode: 0o600 });
-    return answers;
-  }
-}
 
 function printResults(results) {
   const total = results.length;
@@ -68,18 +23,18 @@ function printResults(results) {
 }
 
 async function main() {
-  console.log('LDAP Look-See');
-  const config = await getConfig();
-  const { filter } = await inquirer.prompt(filterQuestions);
-  const ldap = new SimpleLDAPSearch(config);
-
   try {
+    console.log('LDAP Look-See');
+    const config = await getConfig();
+    const { filter } = await inquirer.prompt(filterQuestions);
+    const ldap = new SimpleLDAPSearch(config);
+
     const results = await ldap.search(filter);
     printResults(results);
+    ldap.destroy();
   } catch (err) {
-    // console.error(err);
+    console.error(err);
   }
-  ldap.destroy();
 }
 
 main();
