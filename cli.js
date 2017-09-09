b5c119076601e114b846352bf1bc53c302765daf
James Johnson <james_johnson@mcad.edu>
2017-09-08 20:34:21 -0500

    Code cleanup

--- cli.js
+++ cli.js
@@ -1,9 +1,5 @@
 #!/usr/bin/env node -r reify
 
-// const inquirer = require('inquirer');
-// const SimpleLDAPSearch = require('simple-ldap-search').default;
-// const getConfig = require('./lib/getConfig');
-
 import inquirer from 'inquirer';
 import SimpleLDAPSearch from 'simple-ldap-search';
 import getConfig from './lib/getConfig';
@@ -19,16 +15,6 @@ const filterQuestions = [
   },
 ];
 
-// function printResults(results) {
-//   const total = results.length;
-//   console.log(`${total} RESULTS\n`);
-
-//   results.forEach((result, i) => {
-//     console.log(`\n[${i}/${total}]`);
-//     console.log(result);
-//   });
-// }
-
 async function main() {
   log('LDAP Look-See');
   const config = await getConfig();
@@ -37,15 +23,13 @@ async function main() {
   await ldap
     .search(filter)
     .then(prettifyLDAPResults)
-    .catch((err) => {
-      console.error(err.message);
-    });
+    .catch(err => log(err.message));
   ldap.destroy();
 }
 
 // log unhandled rejections
 process.on('unhandledRejection', (reason, p) => {
-  console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
+  log('Unhandled Rejection at: Promise', p, 'reason:', reason);
 });
 
 main();
